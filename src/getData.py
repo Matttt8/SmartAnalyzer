60968da62b8a733ea51e1f19a47b54bf36d8c50f
Matttt8 <mantovanimatteo@outlook.com>
2024-06-01 09:11:47 +0200

    Continuo

--- src/getData.py
+++ src/getData.py
@@ -9,3 +9,17 @@ for port in ports:
     portList.append(str(port))
     print(port)
 
+choose = input("Seleziona una porta COM:")
+
+for x in range(0, len(portList)):
+    if portList[x].startswith("COM" + str(choose)):
+        portVar = "COM" + str(choose)
+
+serialInst.baudrate = 9600
+serialInst.port = portVar
+serialInst.open()
+
+while True:
+    if serialInst.in_waiting:
+        packet = serialInst.readline()
+        print(packet.decode('utf'))
--- src/main.py
+++ src/main.py
@@ -1,17 +1,18 @@
 import os
+import getData
 from flask import Flask, send_file
 
 app = Flask(__name__, static_folder='./staticFolder')
 
-@app.route("/")
+@app.route("localhost/")
 def index():
     return send_file('HTML/index.html')
 
-@app.route("/home")
+@app.route("localhost/home")
 def home():
     return send_file('HTML/home.html')
 
-@app.route("/outdoors")
+@app.route("localhost/outdoors")
 def outdoors():
     return send_file('HTML/outdoors.html')
 
